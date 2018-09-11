c0a7285cfa38bbf753414b0efe813d3403ef9be9
user141707 <user141707@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-11 18:09:47 -0400

    Commit

---
+++ flix_demo03/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix_demo03
+//
+//  Created by user141707 on 9/10/18.
+//  Copyright © 2018 user141707. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flix_demo03/NowPlayingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NowPlayingViewController.swift
+//  flix_demo03
+//
+//  Created by user141707 on 9/9/18.
+//  Copyright © 2018 user141707. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
